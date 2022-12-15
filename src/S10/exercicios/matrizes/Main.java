da08f295c2e822117d16bf3628b77670a63b1ad8
mauriciojdk <mauriciojdk@gmail.com>
2022-12-15 20:45:13 -0300

    Exercicio de matrizes finalizado

--- src/S10/exercicios/matrizes/Main.java
+++ src/S10/exercicios/matrizes/Main.java
@@ -23,17 +23,17 @@ public class Main {
             for (int j=0; j<mat[i].length; j++){
                 if (mat[i][j] == t){
                     System.out.println("Posição " + i + "; " + j);
-                    if (mat[i][(j-1)] != -1) {
-                        System.out.println("Left: " + mat[i][(j-1)]);
+                    if (j > 0) {
+                        System.out.println("Left: " + mat[i][j-1]);
                     }
-                    if (mat[i][(j+1)] != -1) {
-                        System.out.println("Right: " + mat[i][(j+1)]);
+                    if (i > 0) {
+                        System.out.println("Up: " + mat[i-1][j]);
                     }
-                    if (mat[(i+1)][j] != -1) {
-                        System.out.println("Down: " + mat[(i+1)][j]);
+                    if (j < mat[i].length-1) {
+                        System.out.println("Right: " + mat[i][j+1]);
                     }
-                    if (mat[(i-1)][j] != -1) {
-                        System.out.println("Left: " + mat[(i-1)][j]);
+                    if (i < mat.length-1) {
+                        System.out.println("Down: " + mat[i+1][j]);
                     }
                 }
             }

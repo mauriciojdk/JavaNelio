984c476a4b84d9fe1dc0bab13860fe7ccf403e75
mauriciojdk <mauriciojdk@gmail.com>
2022-12-19 23:19:37 -0300

    Comandos e manipulações de datas

---
+++ src/S10/exercicios/datas/Demo.java
@@ -0,0 +1,34 @@
+package S10.exercicios.datas;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Demo {
+    public static void main(String[] args) {
+        //DEMONSTRAÇÃO
+
+        //Criando formatação
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        //Instanciando tempo no momento de execução
+        LocalDate d01 = LocalDate.now();
+        LocalDateTime d02 = LocalDateTime.now();
+        Instant d03 = Instant.now();
+
+        //Convertendo texto ISO-8601 em objeto
+        LocalDate d04 = LocalDate.parse("2017-05-20");
+        LocalDateTime d05 = LocalDateTime.parse("2017-05-20T02:45:26");
+        Instant d06 = Instant.parse("2017-05-20T02:45:26Z");
+
+        LocalDate d07 = LocalDate.parse("20/07/2022", fmt1);
+        LocalDateTime d08 = LocalDateTime.parse("20/07/2022 01:30", fmt2);
+
+        //Instanciando passando data separada
+        LocalDate d09 = LocalDate.of(2022, 12, 19);
+        LocalDateTime d10 = LocalDateTime.of(2022, 12, 19, 1,0);
+
+    }
+}
---
+++ src/S10/exercicios/datas/Text.java
@@ -0,0 +1,46 @@
+package S10.exercicios.datas;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class Text {
+    public static void main(String[] args) {
+
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+        Instant d06 = Instant.parse("2022-07-22T01:30:26Z");
+
+        DateTimeFormatter fmt1  = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2  = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter fmt3  = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+
+        /*System.out.println("d04 = " + d04.format(fmt1));
+        System.out.println("d04 = " + fmt1.format(d04));
+
+        System.out.println("d05 = " + d05.format(fmt2));
+        System.out.println("d06 = " + fmt3.format(d06));*/
+
+        //Mostrar listas de fuso-horarios customizados
+        /*for (String s : ZoneId.getAvailableZoneIds()){
+            System.out.println(s);
+        }*/
+
+        //Convertendo data global em local
+        LocalDate r1 = LocalDate.ofInstant(d06, ZoneId.systemDefault());
+        LocalDate r2 = LocalDate.ofInstant(d06, ZoneId.of("Portugal"));
+        LocalDateTime r3 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
+
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+
+        //Pegando dia, mês e ano das datas locais
+        System.out.println("d04 dia = " + d04.getDayOfMonth());
+        System.out.println("d04 mês = " + d04.getMonth());
+        System.out.println("d04 ano = " + d04.getYear());
+
+    }
+}

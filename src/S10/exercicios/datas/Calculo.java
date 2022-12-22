d68a6ec9feec7bf1b88c696e03b811953ab8250d
mauriciojdk <mauriciojdk@gmail.com>
2022-12-21 22:39:44 -0300

    Modulo data finalizado, inicio enum

---
+++ src/S10/exercicios/datas/Calculo.java
@@ -0,0 +1,35 @@
+package S10.exercicios.datas;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class Calculo {
+    public static void main(String[] args) {
+
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+        Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
+
+        LocalDate pastWeekLocalDate = d04.minusDays(7);
+        LocalDate nextWeekLocalDate = d04.plusDays(7);
+
+        System.out.println(pastWeekLocalDate);
+        System.out.println(nextWeekLocalDate);
+
+        LocalDateTime pastWeekLocalDateTime = d05.minusDays(7);
+        LocalDateTime nextWeekLocalDateTime = d05.plusDays(7);
+
+        Instant pastWeekInstant = d06.minus(7, ChronoUnit.DAYS);
+        Instant nextWeekInstant = d06.plus(7, ChronoUnit.DAYS);
+
+        Duration t1 = Duration.between(pastWeekLocalDateTime, d05);
+        Duration t2 = Duration.between(pastWeekLocalDate.atStartOfDay(), d04.atStartOfDay());
+
+        System.out.println(t1.toDays());
+        System.out.println(t2.toDays());
+
+    }
+}
